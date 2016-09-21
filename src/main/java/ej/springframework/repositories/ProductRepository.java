efbf9e7d0f0ca37550dc7282f55f5d6f8f97d51e
Eunsoo Jung <hi.michael.jung@gmail.com>
2016-09-21 12:51:36 -0700

    Applied Spring DATA JPA  CRUD Repositories

---
+++ src/main/java/ej/springframework/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package ej.springframework.repositories;
+
+import ej.springframework.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by eunsoojung on 9/21/16.
+ */
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
---
+++ src/main/java/ej/springframework/services/reposervices/ProductServiceRepoImpl.java
@@ -0,0 +1,48 @@
+package ej.springframework.services.reposervices;
+
+import ej.springframework.domain.Product;
+import ej.springframework.repositories.ProductRepository;
+import ej.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eunsoojung on 9/21/16.
+ */
+@Service
+@Profile("springdatajpa")
+public class ProductServiceRepoImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add); // fun with Java 8
+        return products;
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product domainObject) {
+        return productRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.delete(id);
+    }
+}
