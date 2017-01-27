7d4f9a7410e6d14e27c7c9939a14c4f58e6d5dff
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-01-27 01:20:30 -0800

    Add Section 16: Remove Spring Boot and Lecture 82:  Create new H2 console configuration

---
+++ src/main/java/ej/springframework/config/H2ConsoleConfig.java
@@ -0,0 +1,20 @@
+package ej.springframework.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by eunsoojung on 1/27/17.
+ */
+@Configuration
+public class H2ConsoleConfig {
+
+    @Bean
+    public ServletRegistrationBean h2Console() {
+        String path = "/h2-console/";
+        String urlMapping = (path.endsWith("/") ? path + "*" : path + "/*");
+        return new ServletRegistrationBean(new WebServlet(), urlMapping);
+    }
+}
