b5a9173e171049ac2070c5681a51fc9389581047
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-01-27 00:46:00 -0800

    Add Section 16: Remove Spring Boot and Lecture 81: Remove H2 spring boot autoconfiguration

---
+++ src/main/java/ej/springframework/config/H2DataSourceConfig.java
@@ -0,0 +1,22 @@
+package ej.springframework.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by eunsoojung on 1/27/17.
+ */
+@Configuration
+public class H2DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+}
