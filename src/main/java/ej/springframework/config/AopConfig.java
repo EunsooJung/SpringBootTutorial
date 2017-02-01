71508d7923eebd2a3d64aa3138ddd4c4d5cb670f
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-01-31 16:48:42 -0800

    Add Section 16: Remove Spring Boot and Lecture 85: Spring AOP Configuration

---
+++ src/main/java/ej/springframework/config/AopConfig.java
@@ -0,0 +1,13 @@
+package ej.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by eunsoojung on 1/31/17.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+
+}
