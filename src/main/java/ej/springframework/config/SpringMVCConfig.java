0323534caca951d10d7175589792cca98bc408d3
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-03-01 02:07:50 -0800

    Add Section 16: Remove Spring Boot and Lecture 87: Spring MVC Configuration

---
+++ src/main/java/ej/springframework/config/SpringMVCConfig.java
@@ -0,0 +1,13 @@
+package ej.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by eunsoojung on 3/1/17.
+ */
+@EnableWebMvc
+@Configuration
+public class SpringMVCConfig {
+
+}
