42c0017e92b65477892bba1529765238e4916002
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-03-01 14:15:42 -0800

    Add Section 16: Remove Spring Boot and Lecture 88: Annotation driven transaction Manager

--- src/main/java/ej/springframework/config/H2DataSourceConfig.java
+++ src/main/java/ej/springframework/config/H2DataSourceConfig.java
@@ -3,12 +3,18 @@ package ej.springframework.config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 /**
  * Created by eunsoojung on 1/27/17.
  */
+// S16_L88_1.*
+@EnableTransactionManagement
 @Configuration
-public class H2DataSourceConfig {
+public class H2DataSourceConfig implements TransactionManagementConfigurer{
 
     @Bean
     public DataSource dataSource() {
@@ -18,5 +24,17 @@ public class H2DataSourceConfig {
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
+    }
+
+    // S16_L88_1.5: Transaction Manager by Spring
+    @Bean
+    public PlatformTransactionManager txManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    // S16_L88_1.4
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return txManager();
     }
 }
